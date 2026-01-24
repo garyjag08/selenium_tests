2b6bc8237db8ba8744c5ec9f5dcd622ae4f7dc9d
garyjag08 <48196744+garyjag08@users.noreply.github.com>
2026-01-24 13:56:53 -0800

    Add SimpleTest for Selenium page title validation
    This test navigates to a specified URL and asserts that the page title is not empty.

---
+++ SimpleTest.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumDemo
+{
+    public class SimpleTest
+    {
+        [Test]
+        public void PrintTitle()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--headless=new");
+            options.AddArgument("--no-sandbox");
+            options.AddArgument("--disable-dev-shm-usage");
+
+            using var driver = new ChromeDriver(options);
+
+            driver.Navigate().GoToUrl("https://www.accountplusfinance.com/education/bonds");
+            string title = driver.Title;
+
+            TestContext.WriteLine("TITLE: " + title);
+
+            Assert.IsNotEmpty(title);
+        }
+    }
+}
