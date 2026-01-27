0178d9139458b93e8c51a15bf4ea54173cad54dd
garyjag08 <48196744+garyjag08@users.noreply.github.com>
2026-01-27 11:41:26 -0800

    Smoke test
    Simple smoke test to ensure web page loads within 10 seconds

--- SimpleTest.cs
+++ SimpleTest.cs
@@ -1,13 +1,16 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
 
 namespace SeleniumDemo
 {
     public class SimpleTest
     {
         [Test]
-        public void PrintTitle()
+        public void BodyElementIsVisible()
         {
             var options = new ChromeOptions();
             options.AddArgument("--headless=new");
@@ -17,9 +20,15 @@ namespace SeleniumDemo
             using var driver = new ChromeDriver(options);
 
             driver.Navigate().GoToUrl("https://www.accountplusfinance.com/education/bonds");
-            string title = driver.Title;
 
-            TestContext.WriteLine("TITLE: " + title);
+            // Wait up to 10 seconds for <body> to be present and visible
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            var body = wait.Until(ExpectedConditions.ElementIsVisible(By.TagName("body")));
+
+            bool isVisible = body.Displayed;
+
+            TestContext.WriteLine("BODY VISIBLE: " + isVisible);
+            Assert.IsTrue(isVisible, "The <body> element did not load or is not visible.");
         }
     }
 }
