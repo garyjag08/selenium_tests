dd5d217508157f84ec29d3f6133fd58eb0877283
garyjag08 <48196744+garyjag08@users.noreply.github.com>
2026-01-31 14:43:49 -0800

    Add DriverFactory for headless Chrome WebDriver
    Added driver factory module to build the driver

---
+++ DriverFactory.cs
@@ -0,0 +1,15 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumDemo{
+  public class BuildDriver {
+    public IWebDriver InitializeDriver(){
+        var options = new ChromeOptions();
+        options.AddArgument("--headless=new");
+        options.AddArgument("--no-sandbox");
+       options.AddArgument("--disable-dev-shm-usage");
+      var driver = new ChromeDriver(options);
+      return driver; 
+    } // InitializeDriver      
+  }
+}
