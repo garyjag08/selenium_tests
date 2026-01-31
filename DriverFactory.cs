7ca4201db6f5c85af5ab83060ac12d128e9d18af
garyjag08 <48196744+garyjag08@users.noreply.github.com>
2026-01-31 14:58:25 -0800

    Rename BuildDriver class to DriverFactory

--- DriverFactory.cs
+++ DriverFactory.cs
@@ -2,7 +2,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace SeleniumDemo{
-  public class BuildDriver {
+  public class DriverFactory {
     public IWebDriver InitializeDriver(){
         var options = new ChromeOptions();
         options.AddArgument("--headless=new");
