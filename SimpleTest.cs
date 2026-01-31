e77483f700889cbe38a2f8bb6b9e66ed25d64472
garyjag08 <48196744+garyjag08@users.noreply.github.com>
2026-01-31 14:56:35 -0800

    Refactor SimpleTest to use class-level WebDriver

--- SimpleTest.cs
+++ SimpleTest.cs
@@ -1,36 +1,39 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using System;
 
 namespace SeleniumDemo
 {
     public class SimpleTest
     {
+        private IWebDriver _driver;
+        private DriverFactory _factory;
+        private SmokeTest _smoke;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _factory = new DriverFactory();
+            _smoke = new SmokeTest();
+
+            _driver = _factory.CreateChromeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+
         [Test]
         public void BodyElementIsVisible()
         {
-            var options = new ChromeOptions();
-            options.AddArgument("--headless=new");
-            options.AddArgument("--no-sandbox");
-            options.AddArgument("--disable-dev-shm-usage");
+            bool result = _smoke.IsBodyVisible(
+                _driver,
+                "https://www.accountplusfinance.com/education/futures"
+            );
 
-            using var driver = new ChromeDriver(options);
-
-            driver.Navigate().GoToUrl("https://www.accountplusfinance.com/education/futures");
-
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-
-            // Wait for <body> to exist and be displayed
-            var body = wait.Until(d =>
-            {
-                var element = d.FindElement(By.TagName("body"));
-                return element.Displayed ? element : null;
-            });
-
-            TestContext.WriteLine("BODY VISIBLE: " + body.Displayed);
-            Assert.That(body.Displayed, Is.True, "<body> element is not visible");
+            Assert.That(result, Is.True, "Smoke test failed: <body> was not visible.");
         }
     }
 }
+
